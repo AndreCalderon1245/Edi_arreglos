0b47af6a49c25d4dfd95486671e07250a10b0873
caamaledgar <caamal.edgar@gmail.com>
2021-01-25 17:56:19 -0600

    Clase de Arreglos Unidimensionales

--- EDI-Arreglos/src/edi/arreglos/EDIArreglos.java
+++ EDI-Arreglos/src/edi/arreglos/EDIArreglos.java
@@ -16,7 +16,33 @@ public class EDIArreglos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola MUndo GIT");
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int[] bnums = (int[]) nums.clone() ;
+        
+        System.out.println("FOR i");
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            System.out.println(num);
+            
+        }
+
+        System.out.println("For each");
+        for (int num : nums) {
+            System.out.println(num);
+        }
+
+        System.out.println("Clone");
+        for (int bnum : bnums) {
+            System.out.println(bnum);
+        }
+        
+       int[] sarraycopy;
+        sarraycopy = new int[5];
+
+        System.arraycopy(bnums, 5, sarraycopy, 0,3);
+        for (int i : sarraycopy) {
+            System.out.println(i);
+        }
     }
     
 }
